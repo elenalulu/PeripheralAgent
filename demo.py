783761c9ba51ea64ed2cbd2dad97d40704442741
elenalulu <34589890+elenalulu@users.noreply.github.com>
2025-04-05 20:58:03 +0800

    updated

--- demo.py
+++ demo.py
@@ -2,10 +2,10 @@ import subprocess
 import os
 
 
-folder_path = './scripts'
+folder_path = './demo'
 for root, dirs, files in os.walk(folder_path):
 	for file in files:
 		print (file)
 
-		command = "python ./keymousego/KeymouseGo.py ./scripts/" + file + " --runtimes 1"
+		command = "python ./keymousego/KeymouseGo.py ./demo/" + file + " --runtimes 1"
 		subprocess.run(command, capture_output=True, text=True, shell=True)
