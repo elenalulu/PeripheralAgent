736ef0d9c9532b864b0bda46d44e30b694657ce5
elenalulu <34589890+elenalulu@users.noreply.github.com>
2025-04-17 20:30:47 +0800

    updated

--- wubi_demo/agent_demo_wubi.py
+++ wubi_demo/agent_demo_wubi.py
@@ -2,10 +2,10 @@ import subprocess
 import os
 
 
-folder_path = './scripts_ppt'
+folder_path = './scripts_wubi'
 for root, dirs, files in os.walk(folder_path):
 	for file in files:
 		print (file)
 
-		command = "python ../keymousego/KeymouseGo.py ./scripts_ppt/" + file + " --runtimes 1"
+		command = "python ../keymousego/KeymouseGo.py ./scripts_wubi/" + file + " --runtimes 1"
 		subprocess.run(command, capture_output=True, text=True, shell=True)
